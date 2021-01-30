b33dab514ba3d0a17b076bf9182491afbce0dcdf
Tony Watkins <tony@llifion.net>
2021-01-30 20:53:24 +0000

    Trying to fix the es2015 code issue by removing the async await fails as even though the code now returns a plain object, that object is a promise and the data will not have been returned when the reducers are executed

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = async () => {
-    const response = await jsonPlaceholder.get('/posts');
+export const fetchPosts =  () => {
+    const response =  jsonPlaceholder.get('/posts');
     return {
         type: 'FETCH_POSTS',
         payload: response
