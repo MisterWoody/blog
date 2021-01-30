cf2f31d216e67ec9ae0fa0ba174612bab43ae490
Tony Watkins <tony@llifion.net>
2021-01-30 21:28:47 +0000

    Refactor our action for fetch posts to make use of the redux thunk middleware returning a function with dispatch and getstate parameters and invoking the dispatch method when the response is complete from our async function

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts =  () => {
-    const response =  jsonPlaceholder.get('/posts');
-    return {
-        type: 'FETCH_POSTS',
-        payload: response
-    };
+export const fetchPosts = () => {
+    return async function(dispatch, getState) {
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response
+        });
+    }
 };
