bdc1216259d48ae65b8e1f9f4ef0da923ff4fcee
Tony Watkins <tony@llifion.net>
2021-02-05 17:02:44 +0000

    Add some simple styling to the user name details displayed in our user header

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -13,7 +13,7 @@ class UserHeader extends React.Component {
         if (!user) {
             return null;
         }
-        return <div>{user.name}</div>
+        return <div className="header">{user.name}</div>
     }
 }
 
