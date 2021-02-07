058c0579a3738103ae285656bc378d21e7931ace
Tony Watkins <tony@llifion.net>
2021-02-07 13:38:35 +0000

    Note - there is a downside to memoization related to no way to retrieve a user once it is memoized without potentially duplication of code

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,6 +8,7 @@ export const fetchPosts = () => async dispatch => {
 
 export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
 
+// Downside - if remote user deatails change, there's no current way to refetch the user from the remote api
 const _fetchUser = _.memoize(async(id, dispatch) => {
         const response = await jsonPlaceholder.get(`/users/${id}`);
         dispatch({ type: 'FETCH_USER', payload: response.data })
